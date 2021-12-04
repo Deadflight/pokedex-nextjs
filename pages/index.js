a8c19a16420f8c30935f8af5881064e10d7cbb60
Carlos Correa <33291776+Deadflight@users.noreply.github.com>
2021-12-04 19:41:01 -0400

    Testing: i18next in Production

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Layout from '@/components/Layout';
 import PokemonCollection from '@/components/PokemonCollection/PokemonCollection';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-
+import nextI18nextConfig from 'next-i18next.config';
 
 export const getStaticProps = async({locale}) => {
-  const i18Conf = (await serverSideTranslations(locale, ['common', 'pokemonTypes']))
+  const i18Conf = (await serverSideTranslations(locale, ['common', 'pokemonTypes'], nextI18nextConfig))
 
   return {
     props: {
