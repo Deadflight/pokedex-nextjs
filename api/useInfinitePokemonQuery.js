d97478caf2b51e378c88583360b438bc8ddf881f
Carlos Correa <33291776+Deadflight@users.noreply.github.com>
2021-12-09 15:37:05 -0400

    Testing

--- api/useInfinitePokemonQuery.js
+++ api/useInfinitePokemonQuery.js
@@ -2,6 +2,7 @@ import { useInfiniteQuery } from "react-query";
 
 export function useInfinitePokemonQuery({fetchOption, queryKey}){
   return useInfiniteQuery([queryKey], fetchOption, {
+      refetchOnMount: true,
       getNextPageParam: (lastPage,pages) => {
         if(pages.length < 1118){
           return pages.length * 20
