d27a2ae4f0966804c18e25e927e35c747c3cb319
Carlos Correa <33291776+Deadflight@users.noreply.github.com>
2021-12-12 15:40:58 -0400

    Added: Errors Page

--- pages/_app.js
+++ pages/_app.js
@@ -20,9 +20,13 @@ function MyApp(props) {
     <CacheProvider value={emotionCache}>
       <Head>
         <title>Pokedex</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <meta charSet="utf-8" key="charSet"/>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" key="font"></link>    
+        <meta property='og:title' content='Pokedex'/>
+        <meta property='og:image' content='//https://github.com/Deadflight/pokedex-nextjs/tree/main/public/pokedex.PNG'/>
+        <meta property='og:description' content='Pokedex using Next.js, i18n,React Query, Styled Components, PokeAPI, SSR, ISR, SSG, Material UI'/>
+        <meta property='og:url' content='//https://pokedex-nextjs-beta.vercel.app'/>
         <noscript id="mui-insertion-point" />
       </Head>
       <QueryProvider>
